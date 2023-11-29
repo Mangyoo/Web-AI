a5296b251aee19e22248bec1002a9aadb29194dc
malewerentz <malewerentz@uni-osnabrueck.de>
2023-11-29 19:40:42 +0100

    inital commit

--- myapp.py
+++ myapp.py
@@ -1,21 +1,15 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, jsonify
 from crawler import WebCrawler
 from indexing import Indexer
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates")
 index_dir = "index_dir"
 indexer = Indexer(index_dir)
 web_crawler = WebCrawler("https://vm009.rz.uos.de/crawl/index.html", "vm009.rz.uos.de", indexer)
 
 @app.route('/')
 def home():
-    return """
-    <form action="/search" method="get" style="margin-top: 20px; text-align: center;">
-    <label for="q" style="font-size: 18px; margin-right: 10px;">Search:</label>
-    <input type="text" id="q" name="q" style="padding: 8px; font-size: 16px; border: 1px solid #ccc; border-radius: 4px;">
-    <input type="submit" value="Search" style="padding: 8px 16px; font-size: 16px; background-color: #4CAF50; color: white; border: none; border-radius: 4px; cursor: pointer;">
-    </form>
-    """
+    return render_template('index.html')
 
 @app.route('/search')
 def search():
@@ -23,13 +17,9 @@ def search():
     if query:
         web_crawler.run_crawler()
         search_results = indexer.search(query)
-        result_html = "<ul>"
-        for result in search_results:  #search results in jinja template
-            result_html += f"<li><a href='{result['url']}'>{result['title']}</a><br>{result['teaser']}</li>"
-        result_html += "</ul>"
-        return result_html
+        return jsonify(search_results)
     else:
-        return "Please enter a search query."
-    
+        return jsonify(message="Please enter a search query.")
+
 if __name__ == '__main__':
     app.run(debug=True)
