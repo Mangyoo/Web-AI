46e5118b6d10d2386244796aea5044d6ed1e1d0f
fmorosov <fmorosov@uni-osnabrueck.de>
2024-01-05 19:06:04 +0100

    added the faster init version

---
+++ recommender_base_with_js_faster/recommender.py
@@ -0,0 +1,92 @@
+# Contains parts from: https://flask-user.readthedocs.io/en/latest/quickstart_app.html
+
+from flask import Flask, render_template, request
+from flask_user import login_required, UserManager, current_user
+
+from models import db, User, Movie, MovieGenre, Link, Tag, Rating
+from read_data import check_and_read_data
+
+# import sleep from python
+from time import sleep
+
+# Class-based application configuration
+class ConfigClass(object):
+    """ Flask application config """
+
+    # Flask settings
+    SECRET_KEY = 'This is an INSECURE secret!! DO NOT use this in production!!'
+
+    # Flask-SQLAlchemy settings
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///movie_recommender.sqlite'  # File-based SQL database
+    SQLALCHEMY_TRACK_MODIFICATIONS = False  # Avoids SQLAlchemy warning
+
+    # Flask-User settings
+    USER_APP_NAME = "Movie Recommender"  # Shown in and email templates and page footers
+    USER_ENABLE_EMAIL = False  # Disable email authentication
+    USER_ENABLE_USERNAME = True  # Enable username authentication
+    USER_REQUIRE_RETYPE_PASSWORD = True  # Simplify register form
+
+    # make sure we redirect to home view, not /
+    # (otherwise paths for registering, login and logout will not work on the server)
+    USER_AFTER_LOGIN_ENDPOINT = 'home_page'
+    USER_AFTER_LOGOUT_ENDPOINT = 'home_page'
+    USER_AFTER_REGISTER_ENDPOINT = 'home_page'
+
+# Create Flask app
+app = Flask(__name__)
+app.config.from_object(__name__ + '.ConfigClass')  # configuration
+app.app_context().push()  # create an app context before initializing db
+db.init_app(app)  # initialize database
+db.create_all()  # create database if necessary
+user_manager = UserManager(app, db, User)  # initialize Flask-User management
+
+
+@app.cli.command('initdb')
+def initdb_command():
+    global db
+    """Creates the database tables."""
+    check_and_read_data(db)
+    print('Initialized the database.')
+
+
+# The Home page is accessible to anyone
+@app.route('/')
+def home_page():
+    # render home.html template
+    return render_template("home.html")
+
+
+# The Members page is only accessible to authenticated users via the @login_required decorator
+@app.route('/movies')
+@login_required  # User must be authenticated
+def movies_page():
+    # String-based templates
+
+    # first 10 movies
+    movies = Movie.query.limit(100).all()
+
+    # only Romance movies
+    # movies = Movie.query.filter(Movie.genres.any(MovieGenre.genre == 'Romance')).limit(10).all()
+
+    # only Romance AND Horror movies
+    # movies = Movie.query\
+    #     .filter(Movie.genres.any(MovieGenre.genre == 'Romance')) \
+    #     .filter(Movie.genres.any(MovieGenre.genre == 'Horror')) \
+    #     .limit(10).all()
+
+    return render_template("movies.html", movies=movies)
+
+
+@app.route('/rate', methods=['POST'])
+@login_required  # User must be authenticated
+def rate():
+    movieid = request.form.get('movieid')
+    rating = request.form.get('rating')
+    userid = current_user.id
+    print("Rate {} for {} by {}".format(rating, movieid, userid))
+    return render_template("rated.html", rating=rating)
+
+
+# Start development web server
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
